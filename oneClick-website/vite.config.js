39775ab3fefc597b6f1c40a490803d6958d3b849
Abdelaziz-LAISSAOUI <azizaissa2004@gmail.com>
2023-10-15 23:46:59 +0200

    set base dict

--- oneClick-website/vite.config.js
+++ oneClick-website/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-    base: '/',
+  base: "./",
+  build: {
     rollupOptions: {
       output: {
         format: 'es', // Make sure it's set to 'es' or 'module'
       },
     },
   },
-})
+},
+)
 
